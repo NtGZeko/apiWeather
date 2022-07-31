32aef9cb1894a4f5616a5ffc3f60dea5a2c3480a
NtGZeko <matheooz@live.fr>
2022-07-31 17:26:12 +0200

    call Api

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
+const APIKEY = 'a1a7d8984d40be6d1c25100fb21f0974';
+let url = `https://api.openweathermap.org/data/2.5/weather?q=Lille&appid=${APIKEY}&units=metric&lang=fr`;
 
+fetch(url).then((response) =>
+  response.json().then((data) => console.log(data))
+);
