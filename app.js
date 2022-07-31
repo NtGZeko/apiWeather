a36893116dc805aa7186f25efd4477149702476a
NtGZeko <matheooz@live.fr>
2022-07-31 19:33:36 +0200

    add data api in querySelector

--- app.js
+++ app.js
@@ -2,5 +2,12 @@ const APIKEY = 'a1a7d8984d40be6d1c25100fb21f0974';
 let url = `https://api.openweathermap.org/data/2.5/weather?q=Lille&appid=${APIKEY}&units=metric&lang=fr`;
 
 fetch(url).then((response) =>
-  response.json().then((data) => console.log(data))
+  response.json().then((data) => {
+    console.log(data);
+    document.querySelector('#city').innerHTML = data.name;
+    document.querySelector('#temp').innerHTML = data.main.temp + '°';
+    document.querySelector('#desc').innerHTML = data.weather[0].description;
+    document.querySelector('#lat').innerHTML = data.coord.lat + '°';
+    document.querySelector('#lon').innerHTML = data.coord.lon + '°';
+  })
 );
